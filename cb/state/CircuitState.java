329fb289278537c17d21d083140a8bdfabce5e6b
sandeep-tukaram <iitm.sandeep@gmail.com>
2025-05-03 18:55:26 +0530

    formatting

--- cb/state/CircuitState.java
+++ cb/state/CircuitState.java
@@ -8,7 +8,6 @@ import retry.RetryThresholdException;
 
 public interface CircuitState {
 
-
     public  <Q, S> Optional<S> handle(Q request) throws CircuitOpenException, InterruptedException, RetryThresholdException;
 
 }
