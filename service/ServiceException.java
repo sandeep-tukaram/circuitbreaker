b27e671e2355badc326f28472fe13b91a43da827
sandeep-tukaram <iitm.sandeep@gmail.com>
2025-05-03 18:45:21 +0530

    implemented constructors for ServiceException

--- service/ServiceException.java
+++ service/ServiceException.java
@@ -2,4 +2,12 @@ package service;
 
 public class ServiceException extends Exception {
 
+    public ServiceException(String msg) {
+        super(msg);
+    }
+
+    public ServiceException(String msg, Throwable throwable) {
+        super(msg, throwable);
+    }
+
 }
