bc7a012114e405d0d779b5f9991a0ee0d9667131
sandeep-tukaram <iitm.sandeep@gmail.com>
2025-05-03 14:21:37 +0530

    RetryException msging and exception embedding

--- RetryThresholdException.java
+++ RetryThresholdException.java
@@ -4,6 +4,10 @@ public class RetryThresholdException extends Exception {
     public RetryThresholdException(String msg) {
         super(msg);
     }
+
+    public RetryThresholdException(String msg, Throwable embeddedException) {
+        super(msg, embeddedException);
+    }
     
 }
 
