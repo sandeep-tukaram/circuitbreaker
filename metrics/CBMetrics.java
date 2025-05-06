b7256391799f2e054fd7ce6216054b2c7c11a4a5
sandeep-tukaram <iitm.sandeep@gmail.com>
2025-05-06 15:15:03 +0530

    metrics success and failure counter fields intiailized

--- metrics/CBMetrics.java
+++ metrics/CBMetrics.java
@@ -1,5 +1,6 @@
 package metrics;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import cb.counters.AggregateCounter;
@@ -7,8 +8,8 @@ import cb.state.CircuitStateEnum;
 
 public class CBMetrics {
 
-    private Map<CircuitStateEnum, AggregateCounter> successCounters;    // Shouldn't include OPEN state.
-    private Map<CircuitStateEnum, AggregateCounter> failureCounters;
+    private Map<CircuitStateEnum, AggregateCounter> successCounters = new HashMap<CircuitStateEnum, AggregateCounter>();    // Shouldn't include OPEN state.
+    private Map<CircuitStateEnum, AggregateCounter> failureCounters = new HashMap<CircuitStateEnum, AggregateCounter>();
 
     public void recordSuccess(CircuitStateEnum circuitState, int successCount) {
         AggregateCounter successAggregator = this.successCounters.getOrDefault(circuitState, new AggregateCounter());
