ef0c634196c244f07ee5d7ddad58f8fb1262c802
sandeep-tukaram <iitm.sandeep@gmail.com>
2025-05-06 13:17:43 +0530

    CircuitBreaker Metrics

---
+++ metrics/CBMetrics.java
@@ -0,0 +1,25 @@
+package metrics;
+
+import java.util.Map;
+
+import cb.counters.AggregateCounter;
+import cb.state.CircuitStateEnum;
+
+public class CBMetrics {
+
+    private Map<CircuitStateEnum, AggregateCounter> successCounters;    // Shouldn't include OPEN state.
+    private Map<CircuitStateEnum, AggregateCounter> failureCounters;
+
+    public void recordSuccess(CircuitStateEnum circuitState, int successCount) {
+        AggregateCounter successAggregator = this.successCounters.getOrDefault(circuitState, new AggregateCounter());
+        successAggregator.increment(successCount);
+        this.successCounters.put(circuitState, successAggregator);
+    }
+
+    public void recordFailure(CircuitStateEnum circuitState, int failCount) {
+        AggregateCounter failAggregator = this.failureCounters.getOrDefault(circuitState, new AggregateCounter());
+        failAggregator.increment(failCount);
+        this.failureCounters.put(circuitState, failAggregator);
+    }
+
+}
